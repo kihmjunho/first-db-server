eb562f49f866293fef87c64b2e7be97027637f4c
kimjunho <kihmjunho@gmail.com>
2023-05-08 00:03:22 +0900

    chore: database setting

--- src/config/appConfig.module.ts
+++ src/config/appConfig.module.ts
@@ -8,6 +8,12 @@ import { EnvironmentVariable } from './environmentVariable.interface';
     ConfigModule.forRoot({
       validationSchema: Joi.object<EnvironmentVariable>({
         PORT: Joi.number().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNCHRONIZE: Joi.boolean().required(),
+        DB_LOGGING: Joi.boolean().required(),
       }),
     }),
   ],
--- src/config/environmentVariable.interface.ts
+++ src/config/environmentVariable.interface.ts
@@ -1,3 +1,10 @@
 export interface EnvironmentVariable {
   PORT: number;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
+  DB_SYNCHRONIZE: boolean;
+  DB_LOGGING: boolean;
 }
