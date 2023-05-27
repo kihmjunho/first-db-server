88439944515c5435d310a760858138706a3d4d2a
kimjunho <kihmjunho@gmail.com>
2023-05-27 23:43:45 +0900

    feat: update board class validatoe

--- src/board/dto/updateBoard.request.dto.ts
+++ src/board/dto/updateBoard.request.dto.ts
@@ -1,4 +1,12 @@
+import { IsOptional, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 export class UpdateBoardRequestDto {
+  @Transform(({ value }) => value.trim())
+  @IsOptional()
+  @Length(1, 15)
   title?: string;
+
+  @IsOptional()
+  @Length(1, 1000)
   description?: string;
 }
